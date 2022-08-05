493f5ea2150f1a9faf8b3aaf0d1cb19605de78a6
MrAxelMG <104875506+MrAxelMG@users.noreply.github.com>
2022-08-05 15:15:20 -0500

    Add files via upload
    Footer (Terminado & Galeria 80% Falta la insercion de iconos en el espacio de las imagenes

---
+++ src/views/Gallery.vue
@@ -0,0 +1,38 @@
+<template>
+<ion-content>
+
+  <ion-grid>
+    <ion-icon class="heart-outline" name="heart-outline"></ion-icon>
+    <ion-col class="img-principal">
+      <img class="img-principal__hpesas" src="/assets/fotos/hombre-p.jpg" alt="" />
+    </ion-col>
+    <div>
+      <ion-col> <img class="img-principal__fotos-row3" src="/assets/fotos/pesas.jpg" alt="" /></ion-col>
+      <ion-col> <img class="img-principal__fotos-row3" src="/assets/fotos/mujer-boxing.jpg" alt="" /></ion-col>
+      <!-- <ion-col> <img class="img-principal__fotos-row3" src="/assets/fotos/pesas.jpg" alt="" /></ion-col> -->
+    </div>
+    <div>
+      <ion-col> <img class="img-principal__fotos-rowlarge" src="/assets/fotos/hombre-c.jpg" alt="" /></ion-col>
+      <div>
+        <ion-col> <img class="img-principal__fotos-row2" src="/assets/fotos/abdominales.jpg" alt="" /> </ion-col>
+        <ion-col> <img class="img-principal__fotos-row2" src="/assets/fotos/llanta.jpg" alt="" /> </ion-col>
+      </div>
+    </div>
+  </ion-grid>
+</ion-content>
+</template>
+
+<script>
+// import {} from '@ionic/vue';
+import { IonCol, IonGrid } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "GaleriaP",
+  components: {
+    IonCol,
+    IonGrid,
+  },
+});
+</script>
+e>
---
+++ src/views/footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="bar bar-subheader bar-royal">
+    <div class="title">LevelFit ©</div>
+  </div>
+
+  <div class="bar-large bar-subheader bar-royal">
+    <!-- <ion-img src="./user.png"></ion-img> -->
+    <!-- <img src="user.png" alt=""> -->
+
+    <div class="SocialComponent">
+      <div class="SocialBar">
+        <ion-icon class="user-icon" name="person-circle"></ion-icon>
+
+        <div class="links">
+          <i class="icon-social"><ion-icon name="logo-facebook"></ion-icon></i>
+          <i class="icon-social"><ion-icon name="logo-twitter"></ion-icon></i>
+          <i class="icon-social"><ion-icon name="logo-instagram"></ion-icon></i>
+          <i class="icon-social"><ion-icon name="mail"></ion-icon></i>
+        </div>
+      </div>
+    </div>
+    <ul class="bar-subheader-container">
+      <li class="bar-subheader-container__name">FAQ</li>
+      <li class="bar-subheader-container__name">Blog</li>
+      <li class="bar-subheader-container__name">Portafolio</li>
+    </ul>
+
+    <ul class="bar-subheader-container">
+      <li class="bar-subheader-container__name">Privacidad</li>
+      <li class="bar-subheader-container__name">Aviso de Privacidad</li>
+      <li class="bar-subheader-container__name">Cookies</li>
+    </ul>
+
+    <!-- <div class="title">Redes </div> -->
+  </div>
+</template>
+
+<script>
+import {} from "@ionic/vue";
+// import { IonFooter, IonTitle, IonToolbar } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FooterP",
+  components: {
+    // IonImg
+    // IonFooter,
+    // IonTitle,
+    // IonToolbar,
+  },
+});
+</script>
