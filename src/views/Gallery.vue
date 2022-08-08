1595e57a5de84eb50f64d86b3380ecbc01d29332
MrAxelMG <104875506+MrAxelMG@users.noreply.github.com>
2022-08-08 10:17:59 -0500

    Add files via upload

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,25 +1,63 @@
 <template>
-<ion-content>
-
-  <ion-grid>
-    <ion-icon class="heart-outline" name="heart-outline"></ion-icon>
-    <ion-col class="img-principal">
-      <img class="img-principal__hpesas" src="/assets/fotos/hombre-p.jpg" alt="" />
-    </ion-col>
-    <div>
-      <ion-col> <img class="img-principal__fotos-row3" src="/assets/fotos/pesas.jpg" alt="" /></ion-col>
-      <ion-col> <img class="img-principal__fotos-row3" src="/assets/fotos/mujer-boxing.jpg" alt="" /></ion-col>
-      <!-- <ion-col> <img class="img-principal__fotos-row3" src="/assets/fotos/pesas.jpg" alt="" /></ion-col> -->
-    </div>
-    <div>
-      <ion-col> <img class="img-principal__fotos-rowlarge" src="/assets/fotos/hombre-c.jpg" alt="" /></ion-col>
-      <div>
-        <ion-col> <img class="img-principal__fotos-row2" src="/assets/fotos/abdominales.jpg" alt="" /> </ion-col>
-        <ion-col> <img class="img-principal__fotos-row2" src="/assets/fotos/llanta.jpg" alt="" /> </ion-col>
+  <ion-content>
+    <ion-grid>
+      <div class="heart-outline__position">
+        <ion-col class="img-principal">
+          <img
+            class="img-principal__hpesas"
+            src="/assets/fotos/hombre-p.jpg"
+            alt=""
+          />
+        </ion-col>
       </div>
-    </div>
-  </ion-grid>
-</ion-content>
+      <div>
+        <ion-col>
+          <img
+            class="img-principal__fotos-row3"
+            src="/assets/fotos/pesas.jpg"
+            alt=""
+        /></ion-col>
+        <ion-col>
+          <img
+            class="img-principal__fotos-row3"
+            src="/assets/fotos/mujer-boxing.jpg"
+            alt=""
+        /></ion-col>
+        <!-- <ion-col> <img class="img-principal__fotos-row3" src="/assets/fotos/pesas.jpg" alt="" /></ion-col> -->
+      </div>
+      <div>
+        <ion-col>
+          <img
+            class="img-principal__fotos-rowlarge"
+            src="/assets/fotos/hombre-c.jpg"
+            alt=""
+        /></ion-col>
+        <div>
+          <ion-col>
+            <img
+              class="img-principal__fotos-row2"
+              src="/assets/fotos/abdominales.jpg"
+              alt=""
+            />
+          </ion-col>
+          <ion-col>
+            <img
+              class="img-principal__fotos-row2"
+              src="/assets/fotos/llanta.jpg"
+              alt=""
+            />
+          </ion-col>
+        </div>
+      </div>
+
+    </ion-grid>
+    <ion-footer>
+
+      <div class="bar bar-subheader bar-royal">
+    <div class="title">LevelFit ©</div>
+  </div>
+    </ion-footer>
+  </ion-content>
 </template>
 
 <script>
@@ -35,4 +73,4 @@ export default defineComponent({
   },
 });
 </script>
-e>
+
